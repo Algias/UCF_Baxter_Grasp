3617b6313d03aadb9cb8725ea737043fe629e1e9
Brandon Gross <BMGross96@knights.ucf.edu>
2016-07-12 15:19:06 -0400

    modified:   launch/baxter_camera.launch 	modified:   scripts/camera_calibrator.py

--- scripts/camera_calibrator.py
+++ scripts/camera_calibrator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import copy
 from geometry_msgs.msg import (
     PointStamped,
@@ -6,16 +7,16 @@ from geometry_msgs.msg import (
     Quaternion,
 )
 import geometry_msgs.msg
+import io
 import numpy
+import re
 import rospy
 from rospy.numpy_msg import numpy_msg
 from std_msgs.msg import Header
 from std_msgs.msg import String
 import sys
 import tf
-import argparse
-import io
-import re
+
 
 #===============================================================================
 # DESCRIPTION
@@ -32,8 +33,6 @@ class CameraCalibrator:
         
     def callback(self, data):
         self.clickedPoint = data;
-        # Uncomment to print out subscriber 
-        # rospy.loginfo(rospy.get_caller_id() + " \n %s", self.clickedPoint)
         
     def listen(self):
         # Subscribe to /clicked_point topic from RVIZ
@@ -45,6 +44,7 @@ class CameraCalibrator:
         print "point 1: ", self.point1
         
     def update_point(self, ClickedPoint):
+        #Returns a point list from a ROS point to use for calculations
             nPoint = [0, 0, 0]
             nPoint[0] = ClickedPoint.x
             nPoint[1] = ClickedPoint.y
@@ -53,7 +53,7 @@ class CameraCalibrator:
             return nPoint
         
     def calibrate(self):
-        print "*****Starting camera calibrator Node"
+        print "*****Starting Camera Calibrator Node"
         rospy.init_node('calibrate_camera',
                       anonymous=True)
         
@@ -64,60 +64,84 @@ class CameraCalibrator:
         calib_list = []
         clicked_list = []
         
-        with io.FileIO("camera_transforms", "w") as outFile:  # creates a file to write out to
+         # creates a file to write out to
+        with io.FileIO("camera_transforms", "w") as outFile: 
             # w opens a file for writing
             rospy.sleep(.5)
+            
             self.listen()
+            
             for x in range(0, 20):
                 print "\n!!!!! Enter for point ",x+1,"!!!!!"
                 n = raw_input()
+                
                 rospy.sleep(.1)
+                
                 self.listen()
+                
                 if n == "":
                     pass
                 else:
                     break
+                
                 # Move the arm?
-                # Click the point / auto transform
-                # transform from clicked pt. to kinect2
+                #Perhaps not for safety, use joint keyboard?
+                
+                # Click the point / auto transform from clicked pt. to kinect2
                 clicked_array = self.update_point(self.clickedPoint.point)
+                
                 # transform from calib_frame to base
                 try:
                     (calib_array, rot) = calib_frame_listener.lookupTransform(
-                                                          'base',
-                                                          'calib_frame',
+                                                          '/base',
+                                                          '/calib_frame',
                                                           rospy.Time(0))
-                except (tf.LookupException,tf.ConnectivityException,tf.ExtrapolationException):
+                except (tf.LookupException,
+                        tf.ConnectivityException,
+                        tf.ExtrapolationException):
                     print "transform error"
                     break
-                # write to a list
+                
+                # write to a list / convert tuple to list
                 calib_array = list(calib_array)
+                
                 print "calib array: ",calib_array
                 print "clicked array: ", clicked_array
+                
+                #Add the current values to the 2D array
                 calib_list.append(calib_array)
                 clicked_list.append(clicked_array)
-            print "calib list: ", calib_list
-            print "clicked list: ", clicked_list
+                
+            print "\n calib list: ", calib_list
+            print "\n clicked list: ", clicked_list
             
+            #Foreach member in the list, write to individual numbers to the file
             count = 0
             for member in calib_list:
                 if count == 0:
                     outFile.write("calib list: \n[")
-                outFile.write(str(calib_list[count][0]) + " "+ str(calib_list[count][1]) +" "+ str(calib_list[count][2])+"; ")  
+                outFile.write(str(calib_list[count][0]) +
+                               " "+ str(calib_list[count][1]) +
+                               " "+ str(calib_list[count][2])+
+                               "; ")  
                 
                 count += 1  
             outFile.write("] \n")
             
+            #Same process as earlier, but with the clicked list
             count = 0
             for member in clicked_list:
                 if count == 0:
                     outFile.write("clicked list: \n[")
-                outFile.write(str(clicked_list[count][0]) + " "+ str(clicked_list[count][1]) +" "+ str(clicked_list[count][2])+"; ")  
+                outFile.write(str(clicked_list[count][0]) +
+                               " "+ str(clicked_list[count][1]) +
+                               " "+ str(clicked_list[count][2])+
+                               "; ")  
                 
                 count += 1  
             outFile.write("]")
             
-        outFile.close()  # close io
+        outFile.close()  # close file
         
         
         
--- scripts/camera_streamer.py
+++ scripts/camera_streamer.py
@@ -2,6 +2,9 @@
 import baxter_interface
 import rospy
 from sensor_msgs.msg import Image
+import sys
+import struct
+import argparse
 
 
 rospy.init_node("my_cam")
@@ -11,5 +14,62 @@ def republish(msg):
             Sends the camera image to baxter's display
         """             
         display_pub.publish(msg)
-sub = rospy.Subscriber('/kinect2/sd/image_color_rect', Image,republish,None,1)
-rospy.spin()
+def streamer(camera):
+    if camera == "head":
+#         left_camera = baxter_interface.CameraController("left_hand_camera")
+#         left_camera.close()
+#         right_camera = baxter_interface.CameraController("right_hand_camera")
+#         right_camera.close()
+        head_camera = baxter_interface.CameraController("head_camera")
+        head_camera.open()
+        camera_name = "head_camera"
+        head_camera.resolution =(960, 600)
+        sub = rospy.Subscriber('/cameras/' + camera_name + "/image", Image,republish,None,1)
+        
+    if camera == "right_hand_camera":
+#         left_camera = baxter_interface.CameraController("left_hand_camera")
+#         left_camera.close()
+        right_camera = baxter_interface.CameraController("right_hand_camera")
+        right_camera.open()
+#         head_camera = baxter_interface.CameraController("head_camera")
+#         head_camera.close()
+        camera_name = "right_hand_camera"
+        right_camera.resolution =(960, 600)
+
+        sub = rospy.Subscriber('/cameras/' + camera_name + "/image", Image,republish,None,1)
+        
+    if camera == "left_hand_camera":
+        left_camera = baxter_interface.CameraController("left_hand_camera")
+        left_camera.open()
+#         right_camera = baxter_interface.CameraController("right_hand_camera")
+#         right_camera.close()
+#         head_camera = baxter_interface.CameraController("head_camera")
+#         head_camera.close()
+        camera_name = "left_hand_camera"
+        left_camera.resolution =(960, 600)
+        sub = rospy.Subscriber('/cameras/' + camera_name + "/image", Image,republish,None,1)
+        
+    if camera == "kinect":
+#         left_camera = baxter_interface.CameraController("left_hand_camera")
+#         left_camera.close()
+#         right_camera = baxter_interface.CameraController("right_hand_camera")
+#         right_camera.close()
+#         head_camera = baxter_interface.CameraController("head_camera")
+#         head_camera.close()
+        sub = rospy.Subscriber('/kinect2/sd/image_color_rect', Image,republish,None,1)
+    
+    rospy.spin()
+def main():
+    arg_fmt = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
+                                     description=main.__doc__)
+    parser.add_argument(
+        '-c', '--camera', choices=['head', 'left_hand_camera', 'right_hand_camera', 'kinect'], required=False,
+        type = str,
+        default="kinect",
+        help="the camera to display on baxter"
+    )
+    args = parser.parse_args(rospy.myargv()[1:])
+    streamer(args.camera)
+if __name__ == '__main__':
+    sys.exit(main())
